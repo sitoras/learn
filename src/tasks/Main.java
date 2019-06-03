68a8ef19eca0cdf7a5cd07b612c8fcec2895d7a3
setora <sitora>
2019-06-03 18:11:12 +0500

    1

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -2,11 +2,17 @@ package tasks;
 
 import tasks.from_codeabbey.Task1;
 import tasks.from_codeabbey.Task2;
+import tasks.from_codeabbey.Task3;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
       //Task1.calculate();
-        Task2.calculate_2();
+        // Task2.calculate_2();
+    Task3.calculate_3();
+
+
     }
 }
--- src/tasks/from_codeabbey/Task3.java
+++ src/tasks/from_codeabbey/Task3.java
@@ -1,21 +1,25 @@
 package tasks.from_codeabbey;
 
+import java.util.Arrays;
+
 public class Task3 {
 
     public static void calculate_3() {
-        public static void calculate_3() {
 
-            int arr1
+        int[] arr1 = {25, 35, 45};
+        int[] arr2 = {45, 100, 5};
+        int[] arr3 = new int[3];
 
-            System.out.println("Введите длину массива");
 
-            for (int i=0; i< count;i++){
-                System.out.println("Введите число "+(i+1));
+        for (int i = 0; i < arr3.length; i++) {
 
-                sum = sum + number;
-            }
+            arr3[i]=arr1[i]+arr2[i];
+                    }
 
-            System.out.println("Total:" +sum);
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.println(Arrays.toString(arr3));
+
         }
+
     }
 }
