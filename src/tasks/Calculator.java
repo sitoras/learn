a8d167f2273525f30fc6aeecbc731183802c7dd7
setora <sitora>
2020-01-28 18:07:58 +0500

    1

---
+++ src/tasks/Calculator.java
@@ -0,0 +1,175 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        String operator;
+
+        System.out.println("Please enter numbers from 1 to 10. You can also use Roman numerals");
+
+        Scanner scanner = new Scanner(System.in);
+        String question;
+        question = scanner.nextLine();
+        question = question.replace(" ", "");
+        calcLine(question);
+
+//        a = scanner.nextInt();
+//        operator = scanner.next();
+////      b = scanner.nextInt();
+//        calc(a, b, operator);
+    }
+
+    private static void calcLine(String questions) {
+        int a, b;
+        boolean type = true;
+
+        // Addition statement
+        if (questions.indexOf('+') != -1) { // Checking the statement:  Is there addition operator?
+            try {
+                a = Integer.parseInt(questions.substring(0, questions.indexOf('+')));
+            } catch (NumberFormatException ex) {
+                type = false;
+                a = romeToDigit(questions.substring(0, questions.indexOf('+')));
+            }
+            try {
+                b = Integer.parseInt(questions.substring(questions.indexOf('+') + 1, questions.length()));
+            } catch (NumberFormatException ex) {
+                type = false;
+                b = romeToDigit(questions.substring(questions.indexOf('+') + 1, questions.length()));
+            }
+
+            if (1 <= a && a <= 10 && 1 <= b && b <= 10) {
+                if (type) {
+                    System.out.println("Total: " + (a + b));
+                } else {
+                    System.out.println("Total: " + DecToRoman.printRoman((a+b)));
+                }
+            } else {
+                System.out.println("Numeral is more than required. Please enter numbers between 1 and 10");
+            }
+
+        }
+
+        //Substraction statement
+        if (questions.indexOf('-') != -1) { // Checking the statement:  Is there substraction operator?
+            try {
+                a = Integer.parseInt(questions.substring(0, questions.indexOf('-')));
+            } catch (NumberFormatException ex) {
+                type = false;
+                a = romeToDigit(questions.substring(0, questions.indexOf('-')));
+            }
+            try {
+                b = Integer.parseInt(questions.substring(questions.indexOf('-') + 1, questions.length()));
+            } catch (NumberFormatException ex) {
+                type = false;
+                b = romeToDigit(questions.substring(questions.indexOf('-') + 1, questions.length()));
+            }
+
+            if (1 <= a && a <= 10 && 1 <= b && b <= 10) {
+                if (type) {
+                    System.out.println("Total: " + (a - b));
+                } else {
+                    System.out.println("Total: " + DecToRoman.printRoman((a-b)));
+                }
+            } else {
+                System.out.println("Numeral is more than required. Please enter numbers between 1 and 10");
+            }
+
+        }
+
+        //Multiplication statement
+        if (questions.indexOf('*') != -1) { // Checking the statement:  Is there multiplication operator?
+            try {
+                a = Integer.parseInt(questions.substring(0, questions.indexOf('*')));
+            } catch (NumberFormatException ex) {
+                type = false;
+                a = romeToDigit(questions.substring(0, questions.indexOf('*')));
+            }
+            try {
+                b = Integer.parseInt(questions.substring(questions.indexOf('*') + 1, questions.length()));
+            } catch (NumberFormatException ex) {
+                type = false;
+                b = romeToDigit(questions.substring(questions.indexOf('*') + 1, questions.length()));
+            }
+
+            if (1 <= a && a <= 10 && 1 <= b && b <= 10) {
+                if (type) {
+                    System.out.println("Total: " + (a * b));
+                } else {
+                    System.out.println("Total: " + DecToRoman.printRoman((a*b)));
+                }
+            } else {
+                System.out.println("Numeral is more than required. Please enter numbers between 1 and 10");
+            }
+
+        }
+
+        //Division statement
+        if (questions.indexOf('/') != -1) { // Checking the statement:  Is there division operator?
+            try {
+                a = Integer.parseInt(questions.substring(0, questions.indexOf('/')));
+            } catch (NumberFormatException ex) {
+                type = false;
+                a = romeToDigit(questions.substring(0, questions.indexOf('/')));
+            }
+            try {
+                b = Integer.parseInt(questions.substring(questions.indexOf('/') + 1, questions.length()));
+            } catch (NumberFormatException ex) {
+                type = false;
+                b = romeToDigit(questions.substring(questions.indexOf('/') + 1, questions.length()));
+            }
+
+            if (1 <= a && a <= 10 && 1 <= b && b <= 10) {
+                if (type) {
+                    System.out.println("Total: " + (a / b));
+                } else {
+                    System.out.println("Total: " + DecToRoman.printRoman((a/b)));
+                }
+            } else {
+                System.out.println("Numeral is more than required. Please enter numbers between 1 and 10");
+            }
+
+        }
+
+    }
+
+//    private static final String[] numerals = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+//    private static final int[] values = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+    private static int romeToDigit(String s){
+        String[] rome_num = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+
+        for (int i = 0; i < rome_num.length; i++) {
+            if (rome_num[i].equals(s)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    private static void calc(int a, int b, String operator) {
+        if (operator.equals("+")) {
+            System.out.println("Total: " + (a + b));
+        }
+
+        if (operator.equals("-")) {
+            System.out.println("Total: " + (a - b));
+        }
+
+        if (operator.equals("/")) {
+            System.out.println("Total: " + (a / b));
+        }
+
+        if (operator.equals("*")) {
+            System.out.println("Total: " + (a * b));
+        }
+    }
+
+}
